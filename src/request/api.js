0dda2c535d0e477608fa2a292eab9057a67a9401
lyh <liuyonghua1996@outlook.com>
2019-05-08 18:20:53 +0800

    喜提接口

---
+++ src/request/api.js
@@ -0,0 +1,4 @@
+import { get, post } from './http'
+
+
+export const one = () => get('https://easy-mock.com/mock/5cd292740f5ffa051a725f88/example/proxy', {});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
@@ -5,10 +6,24 @@
 </template>
 
 <script>
-
-export default {
-  name: 'home',
-  components: {
+  import { one } from '@/request/api';// 导入我们的api接口
+  export default {
+    name: 'home',
+    created () {
+      this.onLoad();
+    },
+    methods: {
+      // 获取数据
+      onLoad() {
+        // 调用api接口，并且提供了两个参数
+        one({
+          // type: 0,
+          // sort: 1
+        }).then(res => {
+          // 获取数据成功后的其他操作
+          console.log(res)
+        })
+      }
+    }
   }
-}
 </script>
